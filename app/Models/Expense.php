effb368d0a3a1609dba36735df61c5d682df32cb
rubel9997 <rubelmahamud9997@gmail.com>
2025-08-22 00:48:07 +0600

    expense and category model created

---
+++ app/Models/Expense.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Expense extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'amount', 'date', 'category_id', 'user_id'];
+
+    protected function casts(): array
+    {
+       return [
+            'date' => 'date',
+            'amount' => 'decimal:2'
+        ];
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
